5b59723e3de90781c80dc53afcd779570f98270b
namtien <namtien.1207@gmail.com>
2023-11-08 10:29:42 +0700

    update

---
+++ terraform/environments/test/main.tf
@@ -0,0 +1,60 @@
+provider "azurerm" {
+  tenant_id       = "${var.tenant_id}"
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  features {}
+}
+terraform {
+  backend "azurerm" {
+    storage_account_name = "tfstate2649518952"
+    container_name       = "tfstate"
+    key                  = "test.terraform.tfstate"
+    access_key           = "UN3DsD2uSdQhUfclG3XMC3Sm7tJaV1mr+n6vyWGckgArvwTQV1tEsVRYIyvFdAG0rHIy33SwUCua+AStuTbUsQ=="
+  }
+}
+
+module "network" {
+  source               = "../../modules/network"
+  address_space        = "${var.address_space}"
+  location             = "${var.location}"
+  virtual_network_name = "${var.virtual_network_name}"
+  application_type     = "${var.application_type}"
+  resource_type        = "NET"
+  resource_group       = "${var.resource_group_name}"
+  address_prefix_test  = "${var.address_prefix_test}"
+}
+
+module "nsg-test" {
+  source           = "../../modules/networksecuritygroup"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "NSG"
+  resource_group   = "${var.resource_group_name}"
+  subnet_id        = "${module.network.subnet_id_test}"
+  address_prefix_test = "${var.address_prefix_test}"
+}
+module "appservice" {
+  source           = "../../modules/appservice"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "AppService"
+  resource_group   = "${var.resource_group_name}"
+}
+module "publicip" {
+  source           = "../../modules/publicip"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "publicip"
+  resource_group   = "${var.resource_group_name}"
+}
+module "vm" {
+  source          = "../../modules/vm"
+  name            = "namtien-VM"
+  location        = "${var.location}"
+  subnet_id       = module.network.subnet_id_test
+  resource_group  = "${var.resource_group_name}"
+  public_ip       = module.publicip.public_ip_address_id
+  admin_username  = "${var.admin_username}"
+  admin_password  = "${var.admin_password}"
+}
---
+++ terraform/environments/test/terraform.tfvars
@@ -0,0 +1,19 @@
+# Azure subscription vars
+subscription_id = "1465c635-13e4-4e4a-a04d-a944d5be0f60"
+client_id = "6047e2d2-eb39-4697-b719-8b9b288ba145"
+client_secret = "3CH8Q~g9_g9YdbyXU6gKW71Hmdtqe3hvVHrpdbnb"
+tenant_id = "8f6f4cc6-288d-4e56-8fb1-d52bddc03fc9"
+
+# Resource Group/Location
+location = "East US"
+resource_group_name = "tiencn-udacity-azdevops-p3"
+application_type = "tiencn"
+
+# Network
+virtual_network_name = "myVirtualNetwork"
+address_space = ["10.5.0.0/16"]
+address_prefix_test = "10.5.1.0/24"
+
+# Virtual machine
+admin_username = "tiencn177"
+admin_password = "MyStr00ngP@$$w0rD"
